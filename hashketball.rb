a2e89f3aafbd18b999cfe15327e3ccf0bf3f0a53
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-06-02 00:09:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,6 @@
-require "pry"
 # Write your code below game_hash
+require "pry"
+
 def game_hash
   {
     home: {
@@ -127,145 +128,119 @@ def game_hash
   }
 end
 
-def num_points_scored(scorer)
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a?(String) 
-        data.split('').each do |data_items|
-        end 
-      elsif data.is_a?(Array)
-        data.each do |element|
-          if element.is_a?(String)
-            element.split('').each do |element_items|
-            end
-          elsif element.is_a?(Hash)
-            element.each do |data_type, player_stat|
-              if player_stat == scorer
-                return element[:points]
-              end 
-            end 
-          end 
-        end 
-      end 
-    end
+def player(name)
+  game_hash.values.each do |team|
+    team.each do |team_stats|
+      team_stats[:players].select { |player| player[:player_name] == name }
+    end 
   end
 end
 
-def shoe_size (player_name)
-    game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a?(String) 
-        data.split('').each do |data_items|
-        end 
-      elsif data.is_a?(Array)
-        data.each do |element|
-          if element.is_a?(String)
-            element.split('').each do |element_items|
-            end
-          elsif element.is_a?(Hash)
-            element.each do |data_type, player_stat|
-              if player_stat == player_name
-                return element[:shoe]
+# Write code here
+def num_points_scored(player_name)
+  game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats.is_a?(Array)
+        team_stats.each do |player_stats|
+          if player_stats.is_a?(Hash)
+            player_stats.each do |player_stat_type, player_detail|
+              if player_detail == player_name
+                return player_stats[:points]
               end 
             end 
           end 
         end 
       end 
-    end
-  end
+    end 
+  end 
+end 
+
+def shoe_size(player_name)
+  game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats.is_a?(Array)
+        team_stats.each do |player_stats|
+          if player_stats.is_a?(Hash)
+            player_stats.each do |player_stat_type, player_detail|
+              if player_detail == player_name
+                return player_stats[:shoe]
+              end 
+            end 
+          end 
+        end 
+      end 
+    end 
+  end 
 end 
 
 def team_colors(team_name)
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data == team_name
-        return game_hash[location][:colors]
+  game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats == team_name
+        return team_details[:colors]
       end 
     end 
   end 
 end 
 
-def team_names 
-  team_array = []
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if attribute == :team_name
-        team_array << data
-      end 
-    end 
+def team_names
+  all_teams = []
+  game_hash.each do |team, team_detail|
+    all_teams.push(team_detail[:team_name])
   end 
-  team_array
+  all_teams
 end 
 
-def player_numbers (team_name)
-  jersey_numbers = []
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data == team_name
-        game_hash[location][:players].each do |players|
-          players.each do |data_type, player_stat|
-            if data_type == :number
-              jersey_numbers << player_stat
+def player_numbers(team_name)
+  all_player_numbers = []
+  game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats.is_a?(Array)
+        team_stats.each do |player_stats|
+          if player_stats.is_a?(Hash)
+            if team_details[:team_name] == team_name
+              all_player_numbers.push(player_stats[:number])
             end 
           end 
-        end
+        end 
       end 
     end 
-  end 
-  jersey_numbers
+  end
+  all_player_numbers
 end 
 
 def player_stats(player_name)
-  stats = {}
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a?(String) 
-        data.split('').each do |data_items|
-        end 
-      elsif data.is_a?(Array)
-        data.each do |element|
-          if element.is_a?(String)
-            element.split('').each do |element_items|
-            end
-          elsif element.is_a?(Hash)
-            element.each do |data_type, player_stat|
-              if player_stat == player_name
-                stats = element
-              end 
+  game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats.is_a?(Array)
+        team_stats.each do |player_stats|
+          if player_stats.is_a?(Hash)
+            if player_stats[:player_name] == player_name
+              return player_stats
             end 
           end 
         end 
       end 
-    end
-  end
-  stats 
-end
-  
-def big_shoe_rebounds
-  shoe_size = 0 
-  rebound = 0 
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a?(String) 
-        data.split('').each do |data_items|
-        end 
-      elsif data.is_a?(Array)
-        data.each do |element|
-          if element.is_a?(String)
-            element.split('').each do |element_items|
-            end
-          elsif element.is_a?(Hash)
-            element.each do |data_type, player_stat|
-              if element[:shoe] > shoe_size
-                shoe_size = element[:shoe]
-                rebound = element[:rebounds]
-              end
-            end 
-          end 
-        end 
-      end 
-    end
-  end
-  rebound
-end
+    end 
+  end 
+end 
 
+def big_shoe_rebounds
+  big_shoe_player = {}
+  big_shoe_size = 0
+    game_hash.each do |team, team_details|
+    team_details.each do |detail_type, team_stats|
+      if team_stats.is_a?(Array)
+        team_stats.each do |player_stats|
+          if player_stats.is_a?(Hash)
+            if player_stats[:shoe] > big_shoe_size
+              big_shoe_size = player_stats[:shoe]
+              big_shoe_player = player_stats
+            end 
+          end 
+        end 
+      end 
+    end 
+  end 
+  big_shoe_player[:rebounds]
+end 
